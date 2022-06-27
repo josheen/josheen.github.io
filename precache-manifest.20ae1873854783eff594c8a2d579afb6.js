86c31180a8a93e0bb5fff23201dd6c65cb6107a9
josheenyen99 <josheen@ualberta.ca>
2022-06-26 23:32:44 -0600

    Updates

---
+++ precache-manifest.20ae1873854783eff594c8a2d579afb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c98633b0da3fa3c081e2",
+    "url": "/static/css/main.2ff881b7.chunk.css"
+  },
+  {
+    "revision": "c98633b0da3fa3c081e2",
+    "url": "/static/js/main.be679285.chunk.js"
+  },
+  {
+    "revision": "454ad46b82c0a070bb3c",
+    "url": "/static/js/runtime~main.4c66592b.js"
+  },
+  {
+    "revision": "e4fc6e64233b7849d795",
+    "url": "/static/js/2.84e2c748.chunk.js"
+  },
+  {
+    "revision": "9c19088db84563606874",
+    "url": "/static/js/3.5526bd81.chunk.js"
+  },
+  {
+    "revision": "6a6a7aecf9b3084733fa2db71acad09a",
+    "url": "/index.html"
+  }
+];
