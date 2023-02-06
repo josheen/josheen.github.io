055b250cf98ee67a8697cbafda1f26b618c2ef22
Jo-Sheen Yen <jo-sheen.yen@garmin.com>
2023-02-05 18:46:42 -0700

    add guitar tuner project to project list
    Change-Id: I8d80a36146651df3a25d69e973cdc70100ee02d4

--- src/data.ts
+++ src/data.ts
@@ -22,6 +22,14 @@ export const projects = [
             "This is a research project sponsored by the Material Engineering Department of the University of Alberta. The goal of this project was to determine the efficacy of ML techniques in the mining process of froth flotation. Here we explored various architectures such as CNNs, auto-encoders, and semi-supervised GANs. ",
         image: "./froth_image.jpg",
         link: "https://github.com/josheenyen99/FrothSoftSensor"
+    },
+    {
+        title: "Python Guitar Tuner",
+        subtitle: "Tkinter, Threading, Matplotlib and pyAudio",
+        description:
+            "A guitar tuner app that I built using tkinter, pyAudio, and Matplotlib. The app is designed to only have a single main frame containing a live plotter and labels to notify the user of detected changes in pitch; the main tuning thread is connected to the frontend with a queue and updated using another thread ran on the mainframe layer. Lastly, the GUI was built with the intention that the tuner implementation is simply a thread and should be treated as a type of interface.",
+        image: "./pyGuitarTuner.png",
+        link: "https://github.com/josheenyen99/pyGuitarTuner"
     }
 ];
 
